c217c2994ad16e06719ebc77d7867656a5f14ce3
Patrick Vézina <patrick@croustille.io>
2021-04-10 12:46:55 -0400

    i18n

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -3,7 +3,6 @@
 return [
 
     'supportedLocales' => [
-        'de' => ['name' => 'German', 'script' => 'Latn', 'native' => 'Deutsch', 'regional' => 'de_DE'],
         'en' => ['name' => 'English', 'script' => 'Latn', 'native' => 'English', 'regional' => 'en_CA'],
         'fr' => ['name' => 'French', 'script' => 'Latn', 'native' => 'Français', 'regional' => 'fr_CA'],
     ],
--- config/translatable.php
+++ config/translatable.php
@@ -13,7 +13,6 @@ return [
     'locales' => [
         'en',
         'fr',
-        'de',
     ],
 
     /*
