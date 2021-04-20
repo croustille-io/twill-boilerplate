57043bc6ecbdbf6c1e8536035421a24e24b48111
Patrick Vézina <patrick@croustille.io>
2021-04-20 14:22:26 -0400

    demo commented out

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h1>{{ $item->title }}</h1>
 
-<div class="w-1/2 mx-auto">
+{{-- <div class="w-1/2 mx-auto">
     {!! CroustilleImage::fullWidth($item, 'preview_image') !!}
-</div>
+</div> --}}
 @stop
